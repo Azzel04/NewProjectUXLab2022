9654602c8f17479d32aeb9f812e79b38a83f8e91
Azzel04 <azzelreyhanth@gmail.com>
2022-06-10 17:03:21 +0700

    Revisi untuk Lab UX 10 Juni 2022 Jam 5 -Azzel

--- app/src/main/java/com/example/homepagemultimedia/HomeFragment.java
+++ app/src/main/java/com/example/homepagemultimedia/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.homepagemultimedia;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,23 +10,39 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class HomeFragment extends Fragment {
-
+public class HomeFragment extends Fragment implements View.OnClickListener{
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+        super.onCreate(savedInstanceState); }
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        Button toProduct = (Button) rootView.findViewById(R.id.buttonHome);
+
+        toProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentProduct = new Intent(getActivity(), ProductActivity.class);
+                ((HomeActivity) getActivity()).startActivity(intentProduct);
+            }
+        });
+
+        return rootView;
+
+    }// end onCreateVi
 
     @Override
     public void onViewCreated(@NonNull @org.jetbrains.annotations.NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
--- app/src/main/java/com/example/homepagemultimedia/MainActivity.java
+++ app/src/main/java/com/example/homepagemultimedia/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,13 +27,18 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String name = username.getText().toString();
                 String pass = password.getText().toString();
-
                 if(name.length() < 3){
                     username.setError("Nama harus lebih dari 3 karakter");
                 }
                 if(pass.isEmpty()){
                     password.setError("Kata Sandi harus diisi");
                 }
+                else{
+                    Toast.makeText(MainActivity.this, "Success!!", Toast.LENGTH_SHORT).show();
+                    Intent toHomePage = new Intent(MainActivity.this,HomeActivity.class);
+                    startActivity(toHomePage);
+                }
+
             }
         });
     }
