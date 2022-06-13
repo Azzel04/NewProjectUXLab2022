7c7e8f4d0ee520fe8cc3158cbd4d8464f842f8c9
cjchristian78 <suicitytokyo@gmail.com>
2022-06-13 19:41:41 +0700

    Fazz

--- app/src/main/java/com/example/homepagemultimedia/DetailActivity.java
+++ app/src/main/java/com/example/homepagemultimedia/DetailActivity.java
@@ -3,6 +3,10 @@ package com.example.homepagemultimedia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        EditText qty = (EditText) findViewById(R.id.editTextNumber);
+        Button confirmation = findViewById(R.id.buttonConfirm);
+
+//        confirmation.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if(qty.getText().toString().length()<0){
+//                    qty.setError("Not enough quantity@");
+//                }else{
+////                    Toast.makeText(DetailActivity.this,DetailActivity.class)
+//                }
+//            }
+//        });
     }
+
+
 }
